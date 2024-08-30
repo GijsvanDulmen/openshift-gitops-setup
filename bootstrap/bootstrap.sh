83aea37bc9557c0e1c64f3fc088e64c961cd118c
Gijs van Dulmen <gijs@vandulmen.net>
2024-08-30 21:34:20 +0200

    Better bootstrap and appproject cluster

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 export KUBECONFIG=/Users/gijs/Documents/repos/proxmox-terraform/openshift/kubeconfig-noingress
+
+helm install gitops-operator ./gitops-operator/values.yaml | kubectl apply -f -
+sleep 5
 helm template . -f ./values.yaml | kubectl apply -f -
