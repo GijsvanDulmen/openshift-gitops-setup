5ff9b596e4e1747823e59ebd715a267a4659faad
Gijs van Dulmen <gijs@vandulmen.net>
2024-08-30 20:01:00 +0200

    Update round 2

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 export KUBECONFIG=/Users/gijs/Documents/repos/proxmox-terraform/openshift/kubeconfig-noingress
-helm template -f ./values.yaml | kubectl apply -f 
+helm template . -f ./values.yaml | kubectl apply -f -
