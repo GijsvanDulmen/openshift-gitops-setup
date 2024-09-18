b231587697025fe70c999d4cd6b0a3c3be73e59f
Gijs van Dulmen <gijs@vandulmen.net>
2024-09-18 20:20:26 +0200

    Fix resources

--- clusters/debug.sh
+++ clusters/debug.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-helm template . -f ./debug.yaml -f ./values.yaml -f ./clusters/dev.yaml --debug
+# helm template . -f ./debug.yaml -f ./values.yaml -f ./clusters/dev.yaml --debug
+
+helm template . -f ./debug.yaml -f ./values.yaml -f ./clusters/dev.yaml --debug -s templates/rbac/namespace-edit.yaml
