98d2e859ca0565f6f48d2dce94fb085b5bbe2635
Gijs van Dulmen <gijs@vandulmen.net>
2025-02-01 15:25:33 +0100

    Fix bootstrap

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-export KUBECONFIG=/Users/gijs/Documents/repos/proxmox-terraform/openshift/kubeconfig-noingress
+# export KUBECONFIG=/Users/gijs/Documents/repos/proxmox-terraform/openshift/kubeconfig-noingress
+export KUBECONFIG=/Users/gijs/Documents/repos/proxmox-terraform/openshift-upi/installation/auth/kubeconfig
+
+# add helm repos
+helm repo add redhat-cop https://redhat-cop.github.io/helm-charts
 
 # make more room on cluster
 kubectl patch --type=merge scheduler cluster -p '{"spec":{"mastersSchedulable":true}}'
@@ -13,8 +17,8 @@ kubectl get crd gateways.gateway.networking.k8s.io &> /dev/null || \
 cd ../operators/gitops-operator
 helm dependency build
 helm template . \
-    -f ../../openshift-gitops-config/config/onprem-dev/values.yaml \
-    -f ../../openshift-gitops-config/config/onprem-dev/onprem-dev-1/values.yaml \
+    -f ../../../openshift-gitops-config/config/onprem-dev/values.yaml \
+    -f ../../../openshift-gitops-config/config/onprem-dev/onprem-dev-1/values.yaml \
     --name-template gitops-operator | kubectl apply -f -
 
 cd ../../bootstrap/
