437eca1db4672caca99ac8865985eb5d6442021c
Gijs van Dulmen <gijs@vandulmen.net>
2025-02-18 21:52:40 +0100

    Add disconnected source option

--- clusters/debug.sh
+++ clusters/debug.sh
@@ -5,4 +5,4 @@ helm template . -f ./debug.yaml \
     -f ../../openshift-gitops-config/config/values.yaml \
     -f ../../openshift-gitops-config/config/onprem-dev/values.yaml \
     -f ../../openshift-gitops-config/config/onprem-dev/onprem-dev-1/values.yaml \
-    --debug -s templates/tenants.yaml
+    --debug -s templates/operators.yaml
