5043af4c6c9ddbfc3c334addb84c5bf4c21c8181
Gijs van Dulmen <gijs@vandulmen.net>
2025-04-21 13:07:43 +0200

    Update NTP for no NTP setup

--- clusters/butane/generate.sh
+++ clusters/butane/generate.sh
@@ -3,5 +3,9 @@
 curl -s https://mirror.openshift.com/pub/openshift-v4/clients/butane/latest/butane-darwin-amd64 --output butane
 chmod +x butane
 
+# real stuff
+./butane 99-master-chrony-conf-override.bu -o ../templates/machineconfig/99-master-chrony-conf-override.yaml
 ./butane 99-worker-chrony.bu -o ../templates/machineconfig/99-worker-chrony.yaml
+
+# testing
 ./butane 101-test-file-gijs.bu -o ../templates/machineconfig/101-test-file-gijs.yaml
